0f93327a5cef4f28bf2fa0d7e36db41ea55f0d61
davidh2k <davidh1990@gmx.de>
2014-09-08 18:36:04 +0200

    added Pxe.js
    Added Pxe.js

---
+++ var/www/openmediavault/js/omv/module/admin/service/pxe/Pxe.js
@@ -0,0 +1,32 @@
+/**
+ * @license     http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author      Ian Moore <imooreyahoo@gmail.com>
+ * @author      Marcel Beck <marcel.beck@mbeck.org>
+ * @author      OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+ * @copyright   Copyright (c) 2011 Ian Moore
+ * @copyright   Copyright (c) 2012 Marcel Beck
+ * @copyright   Copyright (c) 2013-2014 OpenMediaVault Plugin Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ // require("js/omv/WorkspaceManager.js")
+
+OMV.WorkspaceManager.registerNode({
+    id      : "pxe",
+    path    : "/service",
+    text    : _("PXE"),
+    icon16  : "images/",
+    iconSvg : "images/"
+});
