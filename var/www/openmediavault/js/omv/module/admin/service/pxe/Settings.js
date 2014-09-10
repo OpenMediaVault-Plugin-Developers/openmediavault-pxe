fb7066e3d90eb344bfa2759b0c024ae7c5bb42db
davidh2k <davidh1990@gmx.de>
2014-09-10 21:21:37 +0200

    Added Settings.js
    Added Settings.js for review.

---
+++ var/www/openmediavault/js/omv/module/admin/service/pxe/Settings.js
@@ -0,0 +1,68 @@
+/**
+ * This file is part of OpenMediaVault.
+ *
+ * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author    Volker Theile <volker.theile@openmediavault.org>
+ * @copyright Copyright (c) 2009-2014 Volker Theile
+ *
+ * OpenMediaVault is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OpenMediaVault is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+ */
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/form/Panel.js")
+// require("js/omv/form/field/SharedFolderComboBox.js")
+
+/**
+ * @class OMV.module.admin.service.pxe.Settings
+ * @derived OMV.workspace.form.Panel
+ */
+Ext.define("OMV.module.admin.service.pxe.Settings", {
+        extend: "OMV.workspace.form.Panel",
+        requires: [
+                "OMV.form.field.SharedFolderComboBox"
+        ],
+
+        rpcService: "PXE",
+        plugins: [{
+                ptype: "linkedfields",
+                correlations: [{
+                        name: "sharedfolderref",
+                        conditions: [
+                                { name: "enable", value: true }
+                        ],
+                        properties: "!allowBlank"
+                }]
+        },
+
+        getFormItems: function() {
+                var me = this;
+                return [{
+                                xtype: "sharedfoldercombo",
+                                name: "sharedfolderref",
+                                fieldLabel: _("Shared folder"),
+                                allowNone: true,
+                                plugins: [{
+                                        ptype: "fieldinfo",
+                                        text: _("The location of the files to share.")
+                                }]
+                        }]
+                }
+});
+
+OMV.WorkspaceManager.registerPanel({
+        id: "settings",
+        path: "/service/pxe",
+        text: _("Settings"),
+        position: 10,
+        className: "OMV.module.admin.service.pxe.Settings"
+});
