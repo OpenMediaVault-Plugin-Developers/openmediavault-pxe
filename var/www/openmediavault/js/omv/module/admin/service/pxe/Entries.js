ccbf74420014a92af3b628482767107edc898297
Aaron Murray <plugins@omv-extras.org>
2016-02-13 17:55:08 -0600

    update for OMV 3.0

--- var/www/openmediavault/js/omv/module/admin/service/pxe/Entries.js
+++ var/www/openmediavault/js/omv/module/admin/service/pxe/Entries.js
@@ -5,7 +5,7 @@
  * @author      OpenMediaVault Plugin Developers <plugins@omv-extras.org>
  * @copyright   Copyright (c) 2011 Ian Moore
  * @copyright   Copyright (c) 2012 Marcel Beck
- * @copyright   Copyright (c) 2013-2014 OpenMediaVault Plugin Developers
+ * @copyright   Copyright (c) 2013-2016 OpenMediaVault Plugin Developers
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- var/www/openmediavault/js/omv/module/admin/service/pxe/Settings.js
+++ var/www/openmediavault/js/omv/module/admin/service/pxe/Settings.js
@@ -1,9 +1,9 @@
 /**
- * This file is part of OpenMediaVault.
  *
  * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
  * @author    Volker Theile <volker.theile@openmediavault.org>
  * @copyright Copyright (c) 2009-2014 Volker Theile
+ * @copyright Copyright (c) 2013-2016 OpenMediaVault Plugin Developers
  *
  * OpenMediaVault is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
